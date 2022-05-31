861d0924206d6512c9f92c8be46d072a874bee99
Imrnwfi <imranwafi00@gmail.com>
2022-06-01 00:33:18 +0800

    Add Back Button

--- ApplicationLayer/ManageRefundInterface/refundAdmin.php
+++ ApplicationLayer/ManageRefundInterface/refundAdmin.php
@@ -155,9 +155,11 @@ function makeConnect($sql){
 
   </div>
   <center>
+    <!-- Back Button -->
     <form>
     <input type="button" value="Go back!" onclick="history.back()">
     </form>
+
     <h2> Refund request list</h2>
     <form method="post">
     <table class="content-table">
